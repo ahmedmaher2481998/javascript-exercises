e133b9f7c11d78f2f231b3f5e5f803e6fb41576a
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2021-10-25 20:52:43 +0200

    solved 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const ftoc = function() {
+const ftoc = function(fr) {
+  let cel = (fr - 32) / 1.8;  
+  cel = Math.round(cel * 10) / 10.0 ;
+  return cel ;
 
 };
+//Math.round(number * 10) / 10
 
-const ctof = function() {
+const ctof = function(cel) {
+  let fr = (cel * 1.8) + 32  ;
+  fr = Math.round(fr * 10 ) / 10.0 ;
+  return fr ;
 
 };
 

991597838bf31b60b8beaf76d02612b6a71942c9
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2021-10-25 12:56:53 +0200

    01 and 02 is done

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,4 +1,10 @@
-const repeatString = function() {
+const repeatString = function(str , num) {
+    let res = "";
+    if(num < 0 ){return "ERROR"}
+    for(let i =0 ; i < num ; i++){ 
+        res += str;
+    }
+    return res;
 
 };
 

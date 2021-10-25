80082adb2e2df83469ac633ed12608b7b38e486f
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2021-10-25 15:13:01 +0200

    removing item from array[04]

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,13 @@
 const reverseString = function(str) {
+    let letters = str.split('');
+    let reversedStr = "";
+    for(let i = letters.length -1 ; i != -1 ; i--){ 
+        reversedStr += letters[i];
+    }
+    return reversedStr;
 
 };
+console.log(reverseString("hello world !"));
 
 // Do not edit below this line
 module.exports = reverseString;
--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,14 @@
-const removeFromArray = function() {
+const removeFromArray = function(a,b,c,...) {
+    let arr= arguments[0];
+    for(let i = 1 ; i < arguments.length; i++){
+        let index = arr.indexOf(rmElement);
 
+        if(index > 0 ){
+            arr.splice(index,1)
+
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line

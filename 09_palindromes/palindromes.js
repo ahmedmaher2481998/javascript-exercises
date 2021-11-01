2996c1f2c6ed0b9ba855b372df7c7ed1f8287b2e
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2021-11-01 23:44:56 +0200

    solved (9) palindromes'

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
+const palindromes = function (words) {
+    panc = ['.','!',',',' ',"'",'?']
+    let letters = words.split('')
+                                .filter(letter => panc.includes(letter)? false : true)
+                                .map(letter=> letter.toLowerCase());
 
+    let reversed = words.split('')
+                                    .filter(re => panc.includes(re)? false : true).reverse()
+                                    .map(re=> re.toLowerCase());
+
+    for(let i=0; i < letters.length; i++){ 
+        if(letters[i] == reversed[i]) continue;
+        else return false;
+    } 
+    return true;
 };
-
 // Do not edit below this line
 module.exports = palindromes;

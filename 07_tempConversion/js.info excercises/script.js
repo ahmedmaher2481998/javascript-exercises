ce1472d10a0cbcf2c8dc4278a5cfe2500a023bdd
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2021-11-01 20:56:27 +0200

    frsh start last solved excersice (7)

--- 07_tempConversion/js.info excercises/script.js
+++ 07_tempConversion/js.info excercises/script.js
@@ -3,9 +3,9 @@ let user={
     "surname" :"Smith",
 
 };
-delete user.name;
-delete user.surname
-console.log(user)
+// delete user.name;
+// delete user.surname
+// console.log(user)
 
 // user.name='peter';
 
@@ -15,11 +15,56 @@ console.log(user)
 // delete user.name;
 // console.log(user)
 // ******************************************
-function isEmpty(obj){ 
-    for(key in obj){
-        if(key == undefined) return true;
-        if(key in obj) return false;
-        else return true;
-    }
+// function isEmpty(obj){ 
+//     for (key in obj){ 
+//         return false;
+//     }
+//     return true;
+// }
+// let schedule={};
+
+// alert( isEmpty(schedule) ); // false
+// *******************************************
+// let salaries = {
+//     John: 100,
+//   Ann: 160,
+//   Pete: 130
+
+//   }
+//   function sum(obj){ 
+//     let result = 0 ;  
+//     for(key in obj){
+        
+//         result += obj[key];
+//       }
+//       return result;
+
+//   }
+//   console.log(sum(salaries));
+// *********************************************
+function multiplyNumeric(obj){ 
+    for(let key in obj ){ 
+        if(!(typeof(obj[key] ==  'number'))) continue; 
+        obj[key]*= 2;
+        
+    console.log(obj);
+    
 }
-console.log(isEmpty(user))
+/*
+function multiplyNumeric(obj) {
+    for (let key in obj) {
+      if (typeof obj[key] == 'number') {
+        obj[key] *= 2;
+      }
+    }
+  }
+  */
+// before the call
+let menu = {
+    width: 200,
+    height: 300,
+    title: "My menu"
+  };
+  
+  multiplyNumeric(menu); 
+  console.log(menu) 

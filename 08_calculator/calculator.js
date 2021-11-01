d086b3162993b2e8e5ef69f7f6c9be9a4456e3c8
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2021-11-01 22:32:07 +0200

    calc (8) done !

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,39 @@
-const add = function() {
+const add = function(a,b) {
+	return a +b 
+};
+
+const subtract = function(a,b) {
+	return a -b ;
+};
+
+const sum = function(b) {
+  let sum = b.reduce((a,b)=> a +b,0);
+  return sum;
+};
+
+const multiply = function(b) {
+  let total = b.reduce((a,b)=> a*b);
+  return total;
+  } ;
+
+
+const power = function(num, p) {
+  let total = Math.pow(num,p);
+  
+
+  
+  
+  return total;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(num) {
+  let total = 1 ; 
+  
+  for(let i = 1; i <= num; i++){ 
+    total *= i ;
+  }
+  return total;
 	
 };
 

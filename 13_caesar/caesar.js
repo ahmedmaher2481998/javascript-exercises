18af7e59007e195c92c9729d6a82fa498b7fbde1
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2021-11-02 01:27:31 +0200

    caser trail 1 fail

--- 13_caesar/caesar.js
+++ 13_caesar/caesar.js
@@ -1,5 +1,9 @@
-const caesar = function() {
-
+const caesar = function(str,key) {
+ 
+ str = str.split('')
+ str.map((char,indx)=> str.charCodeAt(indx)+key)
+ str = str.join('')
+ return str;
 };
 
 // Do not edit below this line

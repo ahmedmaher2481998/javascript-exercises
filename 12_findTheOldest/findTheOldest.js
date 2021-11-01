0527389db79615644e26918ebc0598e5b69918b8
ahmedmaher2481998 <ahmedmaher2481998@gmail.com>
2021-11-02 00:47:04 +0200

    finished 12 find the oldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,19 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    let theoldest = people.sort((a,b)=> { 
+        let agea = a.yearOfDeath - a.yearOfBirth;
+        let ageb = b.yearOfDeath - b.yearOfBirth;
+        if(!(a.yearOfDeath) ) {
+            let now  = new Date().getFullYear()
+            agea= now - a.yearOfBirth ; 
+        }
+        else if (!b.yearOfDeath) {
+            let now = new Date().getFullYear()
+            ageb = now -b.yearOfBirth;
+        }
+        return agea > ageb ? -1 : 1;
+    })
+    
+    return (theoldest[0])
 
 };
 
